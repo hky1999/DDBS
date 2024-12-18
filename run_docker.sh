b63bf9683e413a5cd9869714a38aa9b4d761ca35
root <root@bic01.lab.pacman-thu.org>
2024-12-18 10:23:47 +0800

    start docker containers with privilege

--- run_docker.sh
+++ run_docker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Machine 0
-docker run -d --rm -p 20000:6379 --name cache0 redis
-docker run -d --rm -p 20001:27017 --name dbms0 mongo
+docker run -d --rm -p 20000:6379 --privileged=true --name cache0 redis
+docker run -d --rm -p 20001:27017 --privileged=true --name dbms0 mongo
 
 # Machine 1
-docker run -d --rm -p 20002:6379 --name cache1 redis
-docker run -d --rm -p 20003:27017 --name dbms1 mongo
+docker run -d --rm -p 20002:6379 --privileged=true --name cache1 redis
+docker run -d --rm -p 20003:27017 --privileged=true --name dbms1 mongo
 
-# Hadoop FS
-cd hadoop
-docker-compose up -d
-cd ..
+# Hadoop FS (use the existing hdfs on bic instead of a new one)
+# cd hadoop
+# docker-compose up -d
+# cd ..
 
 # Show running containers
-docker ps -a
+docker ps
