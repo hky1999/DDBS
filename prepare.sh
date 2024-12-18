99e0d8e7957141555e33320790eae3b2cc62b699
root <root@bic01.lab.pacman-thu.org>
2024-12-18 10:18:57 +0800

    initialize spack and load py3.9; use specific pip

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# setup terminal
+source /spack-bic/share/spack/setup-env.sh
+spack load python@3.9.0
+
 docker pull redis:latest
 docker pull mongo:latest
 
-pip3 install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
